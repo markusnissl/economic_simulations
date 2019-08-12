98ef98ffb2ad8c53e2580f60921d000b5c3e7cc8
Markus Nissl <contact@markusnissl.com>
2019-08-12 16:21:24 +0200

    removed some casts inside compiled code

--- src/main/scala/ecosim/deep/Codegen.scala
+++ src/main/scala/ecosim/deep/Codegen.scala
@@ -7,6 +7,9 @@ import code.__wait
 
 class Codegen[X <: ecosim.runtime.Actor](methodIdMapping: Map[Int, IR.MtdSymbol], actorType: ActorType[X])(implicit val X: CodeType[X]) {
 
+  case class VarWrapper[C](val from: Variable[C], val to:Variable[MutVar[C]])(implicit val A: CodeType[C])
+
+
   val timer = Variable[MutVar[Int]]
   val pos = Variable[MutVar[Int]]
 
@@ -15,12 +18,25 @@ class Codegen[X <: ecosim.runtime.Actor](methodIdMapping: Map[Int, IR.MtdSymbol]
 
   val responseMessage = Variable[MutVar[ResponseMessageInter[Any]]]
 
-  case class VarWrapper(val from: Variable[Any], val to:Variable[MutVar[Any]], val init: OpenCode[Any])
-  var variables:List[VarWrapper] = List()
+  var methodLookupTableVar = Variable[MutVar[Map[Int, Int]]]
+  var methodLookupTable: OpenCode[Map[Int,Int]] = code"Map[Int, Int]()"
+  var methodVariableTable: OpenCode[Map[Int,MutVar[Any]]] = code"Map[Int, MutVar[Any]]()"
+  var methodVariableTableVar = Variable[MutVar[Map[Int, MutVar[Any]]]]
 
-  def generateMutVarInit[R: CodeType](variables:List[VarWrapper], after: OpenCode[R]): OpenCode[R] = variables match {
+  var variables:List[VarWrapper[_]] = List()
+
+  var varSavers = List[VarWrapper[_]]()
+
+
+  def initVar[A, R:CodeType](variable: VarWrapper[A], rest:OpenCode[R]): OpenCode[R] = {
+    import variable.A
+    code"val ${variable.to} = ((MutVar[Any](null)).asInstanceOf[MutVar[A]]); $rest"
+  }
+  def generateMutVarInit[R: CodeType](variables:List[VarWrapper[_]], after: OpenCode[R]): OpenCode[R] = variables match {
     case Nil => code"$after"
-    case (x::xs) => code"val ${x.to} = MutVar(${x.init}); ${generateMutVarInit(xs, after)}"
+    case (x::xs) => {
+      initVar(x, generateMutVarInit(xs, after))
+    }
   }
 
   def generateMethodTable(data: List[(OpenCode[List[() => Unit]], Variable[_], Int)], pos: OpenCode[Int]): OpenCode[Map[Int, Int]] = data match{
@@ -28,26 +44,33 @@ class Codegen[X <: ecosim.runtime.Actor](methodIdMapping: Map[Int, IR.MtdSymbol]
     case Nil => code"Map[Int, Int]()"
   }
 
+  def getMap[A:CodeType](variable: Variable[MutVar[A]], pos: Int): OpenCode[Map[Int, MutVar[A]]] = {
+    code"Map(${Const(pos)} -> ${variable})"
+  }
+
   def generateVariableTable(data: List[(OpenCode[List[() => Unit]], Variable[MutVar[Any]], Int)]): OpenCode[Map[Int, MutVar[Any]]] = data match{
-    case (x::xs) => code"""$${generateVariableTable(xs)} + (${Const(x._3)} -> ${x._2})"""
+    case (x::xs) => code"""(${getMap(x._2, x._3)}) ++ $${generateVariableTable(xs)}"""
     case Nil => code"Map[Int, MutVar[Any]]()"
   }
 
+  //This is not supported :(
+  //var testMethMap:ClosedCode[Map[Int, Variable[MutVar[_]]]] = code"Map[Int,Variable[MutVar[_]]]()"
+
   private def codeGenMethod[A](method: Method[A,_]): (OpenCode[List[() => Unit]], Variable[MutVar[Any]], Int) = {
     import method.A
+
     val methodArgs = Variable[A];
+    //FIXME: Cannot create a map in squid with _ param, therefore have to use any
+    // Cannot define map outside, since methods may have recursive dependency, so lookup has to be made inside squid
+
     val methodArgsMut = Variable[MutVar[Any]];
+//    val methodArgsMut = Variable[MutVar[A]];
     val methodAlgo = method.body(methodArgs)
     val f1 = code"$pos := ((($posSafer!).head) - 1); $posSafer := ($posSafer!).tail; ()"
-    variables = VarWrapper(methodArgs.asInstanceOf[Variable[Any]], methodArgsMut, code"null") :: variables
+    //variables = VarWrapper(methodArgs, methodArgsMut) :: variables
+    variables = VarWrapper(methodArgs.asInstanceOf[Variable[Any]], methodArgsMut) :: variables
     (code"""${this.createCode(methodAlgo).subs(methodArgs).~>(code"($methodArgsMut!).asInstanceOf[A]")} ::: List[() => Unit](() => $f1)""", methodArgsMut, methodIdMapping.map(_.swap).get(method.sym).get)
   }
-
-  var methodLookupTableVar = Variable[MutVar[Map[Int, Int]]]
-  var methodLookupTable: OpenCode[Map[Int,Int]] = code"Map[Int, Int]()"
-  var methodVariableTable: OpenCode[Map[Int,MutVar[Any]]] = code"Map[Int, MutVar[Any]]()"
-  var methodVariableTableVar =  Variable[MutVar[Map[Int, MutVar[Any]]]]
-
 
   def compile(selfRef: X): (Int, Int, Int) => (Int, Int) = {
 
@@ -56,8 +79,7 @@ class Codegen[X <: ecosim.runtime.Actor](methodIdMapping: Map[Int, IR.MtdSymbol]
     val commands = methodCodes.foldLeft(main)((x,y) => code"$x ::: ${y._1}")
 
     methodLookupTable = generateMethodTable(methodCodes, code"$main.length")
-
-    methodVariableTable = generateVariableTable(methodCodes)
+    methodVariableTable = generateVariableTable(methodCodes.map(x => (x._1, x._2.asInstanceOf[Variable[MutVar[Any]]], x._3)))
 
     val finalCode = this.createExec(this.actorType.self, commands)
 
@@ -66,7 +88,6 @@ class Codegen[X <: ecosim.runtime.Actor](methodIdMapping: Map[Int, IR.MtdSymbol]
   }
 
 
-  var varSavers = Map[Variable[_], Variable[MutVar[Any]]]()
 
 
 
@@ -209,13 +230,13 @@ class Codegen[X <: ecosim.runtime.Actor](methodIdMapping: Map[Int, IR.MtdSymbol]
       }
       case fe: Foreach[b, _] => {
         import fe.E
-        val iter = Variable[Iterator[Any]]
-        val iterMut = Variable[MutVar[Any]]
+        val iter = Variable[Iterator[b]]
+        val iterMut = Variable[MutVar[Iterator[b]]]
         val listVal = Variable[b]
-        val listValMut = Variable[MutVar[Any]]
+        val listValMut = Variable[MutVar[b]]
 
-        variables = VarWrapper(iter.asInstanceOf[Variable[Any]], iterMut, code"null") :: variables
-        variables = VarWrapper(listVal.asInstanceOf[Variable[Any]], listValMut, code"null") :: variables
+        variables = VarWrapper(iter, iterMut) :: variables
+        variables = VarWrapper(listVal, listValMut) :: variables
 
         val f1 = code"""$iterMut := ${fe.ls}.iterator; ()"""
         val reset = code"""$pos := ((($posSafer!).head) - 1); $posSafer := ($posSafer!).tail; ()"""
@@ -223,31 +244,34 @@ class Codegen[X <: ecosim.runtime.Actor](methodIdMapping: Map[Int, IR.MtdSymbol]
         val f2 = code"""if($iter.hasNext) {$posSafer := ($pos!) :: ($posSafer!); $listValMut := $iter.next;} else {$pos := ($pos!) + $f3.length;}"""
 
         val finalCode = code"""List(() => $f1, () => $f2) ::: $f3"""
-        val mut1 = finalCode.subs(iter)~>(code"($iterMut!).asInstanceOf[Iterator[Any]]")
-        val mut2 = mut1.subs(listVal)~>(code"($listValMut!).asInstanceOf[b]")
+        val mut1 = finalCode.subs(iter)~>(code"($iterMut!)")
+        val mut2 = mut1.subs(listVal)~>(code"($listValMut!)")
         mut2
       }
       case lb: LetBinding[v, a] => {
         import lb.V
 
-        var bindingMut = Variable[MutVar[Any]]
+        var bindingMut = Variable[MutVar[v]]
 
         var contained = false
-        if (varSavers.contains(lb.bound)) {
-          bindingMut = varSavers(lb.bound)
+
+        val finding = varSavers.find(_.from == lb.bound)
+        if (finding.isDefined) {
+          bindingMut = finding.get.to.asInstanceOf[Variable[MutVar[v]]]
           contained = true
         } else {
-          variables = VarWrapper(lb.bound.asInstanceOf[Variable[Any]], bindingMut, code"null") :: variables
-          varSavers = varSavers + (lb.bound -> bindingMut)
+          val tmp = VarWrapper(lb.bound, bindingMut)
+          variables = tmp :: variables
+          varSavers = tmp :: varSavers
         }
 
         val bindingMutFinal = bindingMut
 
         val met: OpenCode[Unit] = code"""$bindingMutFinal := ${lb.value}; ()"""
-        val met2 = createCode(lb.body).subs(lb.bound).~>(code"($bindingMutFinal!).asInstanceOf[v]")
+        val met2 = createCode(lb.body).subs(lb.bound).~>(code"($bindingMutFinal!)")
 
         if (!contained) {
-          varSavers = varSavers.filter(_._1 != lb.bound)
+          varSavers = varSavers.filter(_.from != lb.bound)
         }
 
 
@@ -256,22 +280,28 @@ class Codegen[X <: ecosim.runtime.Actor](methodIdMapping: Map[Int, IR.MtdSymbol]
       case lb: LetBinding2[v, a] => {
         import lb.V
 
-        var bindingMut2 = Variable[MutVar[Any]]
+        var bindingMut2 = Variable[MutVar[v]]
         var contained = false
 
-        if (varSavers.contains(lb.bound)) {
-          bindingMut2 = varSavers(lb.bound)
+        val finding = varSavers.find(_.from == lb.bound)
+        if (finding.isDefined) {
+          bindingMut2 = finding.get.to.asInstanceOf[Variable[MutVar[v]]]
           contained = true
         } else {
-          variables = VarWrapper(lb.bound.asInstanceOf[Variable[Any]], bindingMut2, code"null") :: variables
-          varSavers = varSavers + (lb.bound -> bindingMut2)
+          val tmp = VarWrapper(lb.bound, bindingMut2)
+          variables = tmp :: variables
+          varSavers = tmp :: varSavers
         }
 
         val bindingMutFinal2 = bindingMut2
 
         val met1 = createCode(lb.value)
-        val met2 = code"""$bindingMutFinal2 := ($returnValue!); ()"""
-        val met3 = createCode(lb.body).subs(lb.bound).~>(code"($bindingMutFinal2!).asInstanceOf[v]")
+        val met2 = code"""$bindingMutFinal2 := (($returnValue!).asInstanceOf[v]); ()"""
+        val met3 = createCode(lb.body).subs(lb.bound).~>(code"($bindingMutFinal2!)")
+
+        if (!contained) {
+          varSavers = varSavers.filter(_.from != lb.bound)
+        }
 
         code"""$met1 ::: List(() => $met2) ::: $met3"""
       }
