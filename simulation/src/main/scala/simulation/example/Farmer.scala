745dc37e20144569dbd29df0afa4e06cb2a79d85
Dusan Milunovic <dusanmilunovic17@gmail.com>
2019-08-23 14:47:24 +0200

    fixed the example

--- simulation/src/main/scala/simulation/example/Farmer.scala
+++ simulation/src/main/scala/simulation/example/Farmer.scala
@@ -2,12 +2,13 @@ package simulation.example
 
 import simulation.core.Actor
 import squid.quasi.lift
+import ecosim.classLifting.SpecialInstructions._
 
 @lift
 class Farmer() extends Actor {
-  var happiness = 0
-  var peers: List[Farmer] = Nil
-  var market: Market = null
+  var happiness: Int = new Integer(1)
+  var peers: List[Farmer] = List[Farmer]()
+  var market: Market = new Market()
 
   def tell(actor: Actor, h: Int): Unit = {
     happiness = happiness - h
@@ -16,6 +17,14 @@ class Farmer() extends Actor {
   def notifyPeers(): Unit = {
     peers.foreach { p =>
       p.tell(this, happiness)
+    }
+  }
+
+  def main() = {
+    while(true) {
+      val testResult = market.sell2(500)
+      println("TEST_VAR", testResult)
+      waitTurns(1)
     }
   }
 
--- simulation/src/main/scala/simulation/example/Market.scala
+++ simulation/src/main/scala/simulation/example/Market.scala
@@ -2,10 +2,11 @@ package simulation.example
 
 import simulation.core.Actor
 import squid.quasi.lift
+import ecosim.classLifting.SpecialInstructions._
 
 @lift
 class Market extends Actor {
-  var goods: List[String] = Nil
+  var goods: List[String] = List[String]()
 
   def sell(unit: Int): Unit = {
     println("Market sells: " + unit)
@@ -21,6 +22,15 @@ class Market extends Actor {
     } else {
       recursiveTest(l.tail)
       println(l.head)
+    }
+  }
+
+  def main() = {
+    while(true) {
+      sell(10)
+      recursiveTest(List(10, 20, 30))
+      handleMessages()
+      waitTurns(1)
     }
   }
 
